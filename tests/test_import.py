21af3f5b220ac181d94086af5078e607b553d031
Sambhav Kothari <sambhavs.email@gmail.com>
2025-01-25 16:23:33 +0000

    Update generated code

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,6 +3,6 @@ import pytest
 
 def test_import():
     try:
-        import cloudcoil.models.kubernetes as kubernetes
+        pass
     except Exception as e:
         pytest.fail(f"Unexpected error: {e}")
